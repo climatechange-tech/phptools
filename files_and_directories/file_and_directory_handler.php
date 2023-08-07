1541151e421b07a9a89969b4c220ce66ff13acc8
climatechange-tech <jagabantxo@gmail.com>
2023-08-07 22:50:27 +0200

    First PHP tools added, such as string handler and file and directory basic handler. All of them have been adapted from Python. PHP-MySQL connection functions pending.

---
+++ files_and_directories/file_and_directory_handler.php
@@ -0,0 +1,466 @@
+<?php
+
+//----------------//
+// Import modules //
+//----------------//
+
+// Import module that finds PHP tools' path //
+//------------------------------------------//
+
+// Server array containing some key paths (localhost) //
+$server_arr = $_SERVER;
+
+// Home path //
+$home_path = $server_arr["HOME"];
+
+// Define the tool finder file path //
+$phptools_finder_file =  "get_phptools_path.php";
+$phptools_finder_path = $home_path . "/" . $phptools_finder_file;
+
+// Perform the importation //
+include($phptools_finder_path);
+fixed_dirpath = return_custom_path();
+
+// Enumerate custom modules and their paths //
+//------------------------------------------//
+
+$custom_mod_path = $fixed_dirpath . "/" . "files_and_directories";
+
+// Perform the custom module importations //
+//----------------------------------------//
+
+$custom_mod_file = $custom_mod_path . "/" . "file_and_directory_paths.php";
+include($custom_mod_file);
+
+
+
+
+//------------------//
+// Define functions //
+//------------------//
+
+// Operations involving files //
+//----------------------------//
+
+/*
+
+def move_files(source_files, destination_directories):
+    
+    source_files = [posixpath_converter(sf, glob_bool=False)
+                    for sf in source_files]
+    
+    if isinstance(source_files, list)\
+    and isinstance(destination_directories, list):
+        
+        for sf, dd in zip(source_files, destination_directories):
+            file_name_nopath = sf.name
+            shutil.move(sf, f"{dd}/{file_name_nopath}")
+            
+    elif isinstance(source_files, list)\
+    and not isinstance(destination_directories, list):
+        
+        for sf in source_files:
+            file_name_nopath = sf.name
+            shutil.move(sf, f"{destination_directories}/{file_name_nopath}")
+    
+    elif not isinstance(source_files, list)\
+    and isinstance(destination_directories, list):
+        
+        for dd in destination_directories:
+            file_name_nopath = source_files.name
+            shutil.move(source_files, f"{dd}/{file_name_nopath}")
+            
+    elif not isinstance(source_files, list)\
+    and not isinstance(destination_directories, list):
+        
+        file_name_nopath = source_files.name
+        shutil.move(source_files, f"{destination_directories}/{file_name_nopath}")
+        
+        
+def copy_files(source_files, destination_directories):
+    
+    source_files = [posixpath_converter(sf, glob_bool=False)
+                    for sf in source_files]
+    
+    if isinstance(source_files, list)\
+    and isinstance(destination_directories, list):
+        
+        for sf, dd in zip(source_files, destination_directories):
+            file_name_nopath = sf.name
+            shutil.copy(sf, f"{dd}/{file_name_nopath}")
+            
+    elif isinstance(source_files, list)\
+    and not isinstance(destination_directories, list):
+        
+        for sf in source_files:
+            file_name_nopath = sf.name
+            shutil.copy(sf, f"{destination_directories}/{file_name_nopath}")
+    
+    elif not isinstance(source_files, list)\
+    and isinstance(destination_directories, list):
+        
+        for dd in destination_directories:
+            file_name_nopath = source_files.name
+            shutil.copy(source_files, f"{dd}/{file_name_nopath}")
+            
+    elif not isinstance(source_files, list)\
+    and not isinstance(destination_directories, list):
+        
+        file_name_nopath = source_files.name
+        shutil.copy(source_files, f"{destination_directories}/{file_name_nopath}")
+    
+*/
+
+// Operations involving directories //
+//----------------------------------//
+
+function make_parent_directories($directory_list) {
+   
+    if (gettype(directory_list === "string")):
+        $directory_list = array($directory_list);
+        
+    foreach ($directory_list as $dir) {
+    	if (!file_exists($dir)) {
+    		mkdir($dir)
+    	}
+    }
+}
+
+
+function copy_entire_directories($source_directories, $destination_directories) {
+	
+	if (file_exists($destination_directories)) {
+		remove_entire_directories($destination_directories);
+	}
+	
+	if (is_dir($source_directories)) {
+		mkdir($destination_directories);
+		$files = scandir($source_directories);
+		
+		foreach ($files as $file) {
+			if ($file != "." && $file != "..") {
+				copy_entire_directories("$source_directories/$file", "$destination_directories/$file");
+			}
+		}
+	}
+	else if (file_exists($source_directories)) {
+		copy($source_directories, $destination_directories);
+	}
+}
+
+        
+function copy_entire_directories($source_directories,
+		                         $destination_directories,
+		                         $files_only=False,
+		                         $recursive_in_depth=True) {
+    
+    if ($files_only === False) {
+        
+        if (gettype(directories) === "array" and gettype($destination_directories) === "array") {
+        	foreach ($source_directories as $sd) {
+            	foreach ($destination_directories as $dd) {
+                    
+                    if (file_exists($dd)) {
+						remove_entire_directories($dd);
+					}
+					
+					if (is_dir($sd)) {
+						mkdir($dd);
+						$files = scandir($sd);
+						
+						foreach ($files as $file) {
+							if ($file != "." && $file != "..") {
+								copy_entire_directories("$sd/$file", "$dd/$file");
+							}
+						}
+					}
+					else if (file_exists($sd)) {
+						copy($sd, $dd);
+					}
+				}
+			}
+		}
+                        
+                    
+        else if (gettype($source_directories) === "array" and gettype($destination_directories) !== "array"):
+
+			if (file_exists($destination_directories)) {
+				remove_entire_directories($destination_directories);
+			}
+        
+            foreach ($source_directories as $sd) {                
+				if (is_dir($sd)) {
+					if (!file_exists($destination_directories)) {
+						mkdir($destination_directories);
+					}
+					$files = scandir($sd);
+					
+					foreach ($files as $file) {
+						if ($file != "." && $file != "..") {
+							copy_entire_directories("$sd/$file", "$destination_directories/$file");
+						}
+					}
+				}
+				else if (file_exists($sd)) {
+					copy($sd, $destination_directories);
+				}
+			}
+                
+           
+        else if (gettype($source_directories) !== "array" and gettype($destination_directories) === "array") {        
+        	foreach ($destination_directories as $dd) {
+                
+                if (file_exists($dd)) {
+					remove_entire_directories($dd);
+				}
+				
+				if (is_dir($source_directories)) {
+					mkdir($dd);
+					$files = scandir($source_directories);
+					
+					foreach ($files as $file) {
+						if ($file != "." && $file != "..") {
+							copy_entire_directories("$source_directories/$file", "$dd/$file");
+						}
+					}
+				}
+				else if (file_exists($source_directories)) {
+					copy($source_directories, $dd);
+				}
+			}  
+        }
+                    
+        else {
+			if (file_exists($destination_directories)) {
+				remove_entire_directories($destination_directories);
+			}
+			
+			if (is_dir($source_directories)) {
+				mkdir($destination_directories);
+				$files = scandir($source_directories);
+				
+				foreach ($files as $file) {
+					if ($file != "." && $file != "..") {
+						copy_entire_directories("$source_directories/$file", "$destination_directories/$file");
+					}
+				}
+			}
+			else if (file_exists($source_directories)) {
+				copy($source_directories, $destination_directories);
+			}
+        }     
+    }
+            
+    else {
+    
+    	$cp_command = "cp -rv '%s'/* '%s'";
+    	
+    	if (gettype($source_directories) === "array" and gettype($destination_directories) === "array") {
+            foreach ($source_directories as $sd) {
+				foreach ($destination_directories as $dd) {
+                    echo shell_exec(sprintf($cp_command, $sd, $dd));
+                }       
+            }
+        }
+        
+        else if (gettype($source_directories) === "array" and gettype($destination_directories) !== "array") {
+            foreach ($source_directories as $sd) {
+                echo shell_exec(sprintf($cp_command, $sd, $destination_directories));
+            }
+        }
+                    
+        else if (gettype($source_directories) !== "array" and gettype($destination_directories) !== "array") {
+			foreach ($destination_directories as $dd) {
+                echo shell_exec(sprintf($cp_command, $source_directories, $dd));
+            }
+        }
+                    
+        else {
+            echo shell_exec(sprintf($cp_command, $source_directories, $destination_directories));
+        }   
+    }        
+}
+
+function remove_entire_directories($directories) {
+
+	if (gettype($directories) === "string") {
+		$directories = array($directories);
+	}
+	
+	foreach ($directories as $dir) {
+		if (is_dir($dir)) {
+			$files = scandir($dir);
+			
+			foreach ($files as $file) {
+				if ($file != "." && $file != "..") {
+					remove_entire_directories("$dir/$file");
+				}
+			}
+			rmdir($dir);
+		}
+		else if (file_exists($dir)) {
+			unlink($dir);
+		}
+	}
+} 
+
+
+def rsync(source_paths,
+          destination_paths,
+          mode="arvh",
+          delete_at_destination=True,
+          source_allfiles_only=False):
+    
+    for sp, dp in zip(source_paths, destination_paths):
+        
+        if delete_at_destination and not source_allfiles_only:
+            
+            zsh_command = f"rsync -{mode} --delete '{sp}' '{dp}'"
+            os.system(zsh_command)
+            
+        elif not delete_at_destination and not source_allfiles_only:
+            zsh_command = f"rsync -{mode} '{sp}' '{dp}'"
+            os.system(zsh_command)
+            
+        elif delete_at_destination and source_allfiles_only:
+            zsh_command = f"rsync -{mode} --delete '{sp}'/* '{dp}'"
+            os.system(zsh_command)
+            
+        elif not delete_at_destination and source_allfiles_only:
+            zsh_command = f"rsync -{mode} '{sp}'/* '{dp}'"
+            os.system(zsh_command)
+    
+/*
+def move_entire_directories(directories, destination_directories):
+    
+    if isinstance(directories, list)\
+    and isinstance(destination_directories, list):
+        
+        for dirc in directories:
+            for dd in destination_directories:
+                shutil.move(dirc, 
+                            dd,
+                            copy_function=shutil.copytree)
+                    
+    elif isinstance(directories, list)\
+    and isinstance(destination_directories, list):
+            
+        len_exts = len(directories)
+        len_dds = len(destination_directories)
+        
+        if len_exts != len_dds:
+            raise ValueError("Extension and destination directory lists "
+                             "are not of the same length.")
+        else:
+            for dirc, dd in zip(directories, destination_directories):
+                shutil.move(dirc,
+                            dd,
+                            copy_function=shutil.copytree)
+                
+    elif isinstance(directories, list)\
+    and not isinstance(destination_directories, list):
+        for dirc in directories:
+            shutil.move(dirc, 
+                        destination_directories, 
+                        copy_function=shutil.copytree)
+                
+    elif not isinstance(directories, list)\
+    and isinstance(destination_directories, list):        
+        for dd in destination_directories:
+            shutil.move(directories,
+                        dd, 
+                        copy_function=shutil.copytree)
+                
+    else:
+        shutil.move(directories, 
+                    destination_directories, 
+                    copy_function=shutil.copytree)
+            
+*/
+      
+
+// Operations involving both files and directories //
+//-------------------------------------------------//
+
+function rename_objects($relative_paths,
+                   		$renaming_relative_paths) {
+
+    /*
+    
+    Function that renames files specified by their absolute paths.
+    
+    In fact, os.rename can also perform the same tasks as shutil.move does,
+    therefore functions 'move_files_byExts_fromCodeCallDir' and
+    'move_files_byFS_fromCodeCallDir', including the fact that,
+    besides moving a directory or file, it includes the option to
+    rename thereof at the destination directory, i.e. altering the
+    ultimate part of the absolute path.
+    
+    However, as a matter of distinguishing among the main usages of the modules,
+    and to invoke simple operations, this function will be used
+    such that each file or directory will be given another name,
+    without altering the absolute path.
+    
+    Parameters
+    ----------
+    relative_paths: str or list
+          String or list of strings that identify the desired files/directories,
+          i.e. the absolute path.
+    renaming_relative_paths : str or list
+          A string of the file/directory name or a list containing
+          several files/directories, i.e the renamed BUT UNALTERED absolute path.
+    
+    This function distinguishes two cases:
+    
+      1. Both file names and directories are lists.
+          Then it is understood that each string file or directory
+          corresponds to another single file or directory and
+          it is renamed as commanded. 
+          File and directory lists have to be of the same length;
+          throws and error otherwise.
+      2. None of them are lists.
+          Then the matching files will simply be renamed.
+          
+    */
+    
+    if (gettype($relative_paths) === "array") and gettype($renaming_relative_paths) === "array") {
+        
+        $len_files = count($relative_paths)
+        $len_rf = count($renaming_relative_paths)
+        
+        if ($len_files != $len_rf) {
+			print("<span style='color:red'>Files and renaming file lists "
+				  "are not of the same length.</span>");
+			exit;
+		}
+         	  
+        else {
+        	$zip_arr = array_map($relative_paths, $renaming_relative_paths);
+        	
+        	for ($i = 0; $i < $len_files; $i++) {
+        		$rp = $zip_arr[$i][0];
+        		$rrp = $zip_arr[$i][1];
+                rename($rp, $rrp);
+            }
+        }   
+    }
+    
+    else if (gettype($relative_paths) !== "array") and gettype($renaming_relative_paths) !== "array") {
+        rename($relative_paths, $renaming_relative_paths)
+    }
+                
+    else {
+        print("<span style='color:red'>Both input arguments must either be "
+         	  "strings or lists simultaneously.</span>");
+		exit;
+	}                    
+}
+
+//--------------------------------------------------------//
+// Get the directory from where this code is being called //
+//--------------------------------------------------------//
+
+$cwd = getcwd();
+$alldoc_dirpath = dirname($fixed_dirpath);
+
+?>
--- setup_repo.php
+++ setup_repo.php
@@ -1,89 +1,160 @@
-"""
-**Note**
+<?php
 
-Run this program if it is the first time using the repository
-or it has been re-cloned.
-"""
+/*
+  **Note**
+  --------
+  Run this program if it is the first time using the repository
+  or it has been re-cloned.
+*/
 
-#----------------#
-# Import modules #
-#----------------#
 
-import os
-from pathlib import Path
-import shutil
+//------------------//
+// Input parameters //
+//------------------//
 
-#------------------#
-# Input parameters #
-#------------------#
+// Server array containing some key paths (localhost) //
+$server_arr = $_SERVER;
 
-# Home path #
-home_path = Path.home()
+// Home path //
+$home_path = $server_arr["HOME"];
 
-# Default path (same as the current one) 
-default_path = Path.cwd()
-default_path_str = str(default_path)
+// Default path (same as the current one) 
+$default_path = getcwd();
 
-# Ask the user if other than the default is preferred #
-custom_path\
-= str(input(f"Where do you want to establish the repository? [{default_path}] >>> "))
+// Path establisher function replica //
+$custom_path_establisher_func = "function return_custom_path() {
+    custom_path = '%s' <br>
+    return custom_path; <br>
+}
+";
 
-# Path establisher function replica #
-custom_path_establisher_func = """def return_custom_path():
-    custom_path = "{}"
-    return custom_path
-"""
-# Repo path retriever file attrs. #
-custom_path_retriever_name = "get_phptools_path.py"
-custom_path_retriever_path = f"{home_path}/{custom_path_retriever_name}"
+// Repo path retriever file attributes //
+$custom_path_retriever_name = "get_phptools_path.php";
+$custom_path_retriever_path = $home_path . "/" . $custom_path_retriever_name;
 
-#----------------#
-# Operation part #
-#----------------#
+//------------------//
+// Define functions //
+//------------------//
 
-lcp = len(custom_path)
+function remove_entire_directories($directories) {
 
-# If 'Enter' key is hit, then set the path to the default one #
-#-------------------------------------------------------------#
+	if (gettype($directories) === "string") {
+		$directories = array($directories);
+	}
+	
+	foreach ($directories as $dir) {
+		if (is_dir($dir)) {
+			$files = scandir($dir);
+			
+			foreach ($files as $file) {
+				if ($file != "." && $file != "..") {
+					remove_entire_directories("$dir/$file");
+				}
+			}
+			rmdir($dir);
+		}
+		else if (file_exists($dir)) {
+			unlink($dir);
+		}
+	}
+} 
 
-if lcp == 0:
-    custom_path = default_path
-    
-# Otherwise, move the repo to the specified path #
-#------------------------------------------------#
 
-else:  
-    
-    # Define custom path with the directory as the current, default one #
-    default_dir = default_path.stem
-    customPathWithDefaultDir = f"{custom_path}/{default_dir}" 
-    
-    """
-    On Windows there is a problem when copying directories,
-    because some path can have double and/or quadruple backlashes and
-    forward slashes mixed, which is not practical to correct.
-    Instead, copy the entire directory and then delete the default one.
-    """
-    shutil.copytree(default_path, customPathWithDefaultDir, dirs_exist_ok=True)
-    
-    print(f"Changed path from '{default_path}' to '{custom_path}'.")
-    
-    os.chdir(f"{default_path}/..")
-    shutil.rmtree(default_path, ignore_errors=True)
-    
-    print(f"Deleted default path '{default_path}', but"
-          "Git version controller directory '.git' will still be present.\n"
-          "Please delete the mentioned path manually.")
-    
-#------------------------------------------------------------------------------#
-# In either case, write a file containing a function that returns the path set #
-#------------------------------------------------------------------------------#
+function copy_entire_directories($source_directories, $destination_directories) {
+	
+	if (file_exists($destination_directories)) {
+		remove_entire_directories($destination_directories);
+	}
+	
+	if (is_dir($source_directories)) {
+		mkdir($destination_directories);
+		$files = scandir($source_directories);
+		
+		foreach ($files as $file) {
+			if ($file != "." && $file != "..") {
+				copy_entire_directories("$source_directories/$file", "$destination_directories/$file");
+			}
+		}
+	}
+	else if (file_exists($source_directories)) {
+		copy($source_directories, $destination_directories);
+	}
+}
+
+
+//----------------//
+// Operation part //
+//----------------//
+
+// If the form is already sent //
+//-----------------------------//
+
+if (isset($_POST["save"])) {
+
+	$custom_path = $_REQUEST["custom_path"];
+	lcp = strlen($custom_path)
+
+	// If the default path is left written, then set the path to the default one //
+	//---------------------------------------------------------------------------//
+
+	if ($custom_path == $default_path) {
+		$custom_path = $default_path;
+	}
+		
+	// Otherwise, move the repo to the specified path //
+	//------------------------------------------------//
+
+	else {
+		
+		// Define custom path with the directory as the current, default one //
+		$default_dir = basename($default_path);
+		$customPathWithDefaultDir = $custom_path . "/". $default_dir; 		
+		
+		$copy_stat = copy_entire_directories($default_path, $customPathWithDefaultDir)
+		
+		if ($copy_stat === True) {
+			printf("Path changed from '%s' to '%s'", $default_path, $custom_path);
+		}
+		else 
+			printf("<span style='color:red'>Could not change path from '%s' to '%s'</span>", $default_path, $custom_path);
+			
+		chdir("$default_path/..")
+		$remove_stat = remove_entire_directories($default_path)
+		
+		if ($remove_stat === True) {
+			printf("Successfully deleted default path '%s'", $default_path);
+		}
+		else {
+			printf("<span style='color:red'>Could not delete default_path '%s'</span>", $default_path);
+		}
+	}
+}
+		
+//------------------------------------------------------------------------------//
+// In either case, write a file containing a function that returns the path set //
+//------------------------------------------------------------------------------//
 
 """
 All modules are optimized so that the file that returns the path of this repo
 is stored at /home directory.
 """
 
-custom_path_retriever = open(custom_path_retriever_path,'w')
-custom_path_retriever.write(custom_path_establisher_func.format(custom_path))
-custom_path_retriever.close()
+$custom_path_retriever = fopen($custom_path_retriever_path,'w');
+fwrite(sprintf($custom_path_establisher_func, $custom_path));
+fclose($custom_path_retriever);
+
+?>
+
+<HTML>
+	<BODY>
+		<FORM ACTION="setup_repo.php" METHOD="POST">
+		
+			Where do you want to establish the repository? 
+			<INPUT TYPE="TEXT" NAME="custom_path" VALUE="[<?php $default_path?>]">
+			
+			<br>
+			<INPUT TYPE="SUBMIT" NAME="set" VALUE="Set">
+			<INPUT TYPE="RESET" VALUE="Reset">
+		</FORM>		
+	</BODY>
+</HTML>
